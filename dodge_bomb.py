2521b5a08f9a7e1b313c8668f2b8a4405178e8c1
okadareo <c0a2203640@edu.teu.ac.jp>
2023-06-20 16:13:41 +0900

    追加1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,88 +1,101 @@
 import random
-import sys
 import pygame as pg
 
+import sys
 
-WIDTH, HEIGHT = 1600, 900
+# 練習４
 delta = {
-    pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, +5),
-    pg.K_LEFT: (-5, 0),
-    pg.K_RIGHT: (+5, 0),
-}
+    pg.K_UP: (0, -1),
+    pg.K_DOWN: (0, +1),
+    pg.K_LEFT: (-1, 0),
+    pg.K_RIGHT: (+1, 0),
+    }
 
-
-def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
+# 練習５
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool,bool]:
     """
-    こうかとんRect，爆弾Rectが画面外 or 画面内かを判定する関数
-    引数：こうかとんRect or 爆弾Rect
-    戻り値：横方向，縦方向の判定結果タプル（True：画面内／False：画面外）
+    オブジェクトが画面内or画面外を判定し、真理値タプルを返す関数
+    引数1：画面SuefaceのRect
+    引数2：こうかとん、または、爆弾SurfaceのRect
+    戻り値：横方向、縦方向のはみ出し判定結果（画面内：True/画面外：False）
     """
     yoko, tate = True, True
-    if rect.left < 0 or WIDTH < rect.right:  # 横方向判定
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
         yoko = False
-    if rect.top < 0 or HEIGHT < rect.bottom:  # 縦方向判定
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
         tate = False
     return yoko, tate
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    screen = pg.display.set_mode((1600, 900))
+    clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
+    
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
-    kk_rct = kk_img.get_rect()
-    kk_rct.center = 900, 400
-    bd_img = pg.Surface((20, 20))  # 練習１
-    bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
-    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
-    x = random.randint(0, WIDTH)
-    y = random.randint(0, HEIGHT)
-    # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
-    bd_rct = bd_img.get_rect()
-    # 爆弾Rectの中心座標を乱数で指定する
-    bd_rct.center = x, y 
-    vx, vy = +5, +5  # 練習２
+    kk_rct = kk_img.get_rect() # 練習４
+    kk_rct.center = 900, 400 # 練習４
 
-    clock = pg.time.Clock()
+    bb_img = pg.Surface((20,20)) # 練習１
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10) # 練習１
+    bb_img.set_colorkey((0,0,0)) # 練習１
+    x, y = random.randint(0,1600), random.randint(0,900) # 練習2
+    vx, vy = +1, +1 # 練習３
+    bb_rct = bb_img.get_rect() # 練習３
+    bb_rct.center = x, y # 練習３
     tmr = 0
+
+    kokaton = {
+        (0, -1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -90),#上
+        (+1, -1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -135),#
+        (+1, 0): pg.transform.rotate(pg.transform.flip(kk_img, False, True), -180),#右
+        (+1, +1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -225),#
+        (0, +1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -270),#下
+        (-1, +1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -315),#
+        (-1, 0): pg.transform.rotate(pg.transform.flip(kk_img, False, False), 0),#
+        (-1, -1): pg.transform.rotate(pg.transform.flip(kk_img, False, False), -40),#
+        }
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
-                return
+                return 0
 
-        if kk_rct.colliderect(bd_rct):  # 練習５
-            print("ゲームオーバー")
-            return   # ゲームオーバー 
-        
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]  # 合計移動量
-        for k, mv in delta.items():
-            if key_lst[k]: 
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
-        kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
- 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, kk_rct)
-        bd_rct.move_ip(vx, vy)  # 練習２
-        yoko, tate = check_bound(bd_rct)
-        if not yoko:  # 横方向に画面外だったら
-            vx *= -1
-        if not tate:  # 縦方向に範囲外だったら
-            vy *= -1
-        screen.blit(bd_img, bd_rct)
-        pg.display.update()
         tmr += 1
-        clock.tick(50)
+
+        # 練習４
+        key_lst = pg.key.get_pressed()
+        for k, mv in delta.items():
+            if key_lst[k]:
+                kk_rct.move_ip(mv)
+                kk_img = kokaton[mv]
+        if check_bound(screen.get_rect(),kk_rct) != (True, True):
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, kk_rct)  #練習４
+        bb_rct.move_ip(vx, vy) # 練習３
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko: # 横方法にはみ出ていたら
+            vx *= -1
+        if not tate: # 縦方向にはみ出ていたら
+            vy *= -1
+        screen.blit(bb_img, bb_rct) # 練習３
+        
+        if kk_rct.colliderect(bb_rct): 
+            return
+
+        pg.display.update()
+        clock.tick(1000)
 
 
-if __name__ == "__main__":
+
+if __name__ == "__main__": 
     pg.init()
+    
     main()
     pg.quit()
     sys.exit()
