abfb82cce378a66041e65a6c9eb02a268f5363fb
okadareo <c0a2203640@edu.teu.ac.jp>
2023-06-20 14:18:47 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,32 +1,29 @@
+import random
 import sys
 import pygame as pg
-import random
+
 
 WIDTH, HEIGHT = 1600, 900
 
-# RGB colors
-RED = (255, 0, 0)
-BLACK = (0, 0, 0)
 
 def main():
-    pg.display.set_caption("逃げろ!こうかとん")
+    pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20, 20))  # 練習１
+    bd_img.set_colorkey((0, 0, 0))  # 黒い部分を透明にする
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    # 爆弾Surface（bd_img）から爆弾Rect（bd_rct）を抽出する
+    bd_rct = bd_img.get_rect()
+    # 爆弾Rectの中心座標を乱数で指定する
+    bd_rct.center = x, y 
+
     clock = pg.time.Clock()
     tmr = 0
-    
-    # Create bomb surface
-    bomb_surface = pg.Surface((20, 20))
-    bomb_surface.set_colorkey(BLACK)
-    pg.draw.circle(bomb_surface, RED, (10, 10), 10)
-
-    # Set initial bomb position randomly
-    bomb_rect = bomb_surface.get_rect()
-    bomb_rect.x = random.randint(0, WIDTH - bomb_rect.width)
-    bomb_rect.y = random.randint(0, HEIGHT - bomb_rect.height)
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -34,10 +31,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-        
-        # Blit bomb surface at its current position
-        screen.blit(bomb_surface, bomb_rect)
-
+        screen.blit(bd_img, bd_rct)
         pg.display.update()
         tmr += 1
         clock.tick(10)
